8ccf45ec6c0481a7c744513bc193d7824d72707e
Di <120228556+codehroll@users.noreply.github.com>
2024-12-08 16:44:30 -0800

    add fetch last record for user in quizId (#3)

--- Kanbas/Quizzes/schema.js
+++ Kanbas/Quizzes/schema.js
@@ -3,10 +3,27 @@ import mongoose from "mongoose";
 const schema = new mongoose.Schema(
   {
     title: { type: String, required: true },
-    course: { type: mongoose.Schema.Types.ObjectId, ref: "CourseModel", required: true }, 
-    type: { type: String, enum: ["Graded Quiz", "Practice Quiz", "Graded Survey", "Ungraded Survey"], default: "Graded Quiz" },
+    course: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "CourseModel",
+      required: true,
+    },
+    type: {
+      type: String,
+      enum: [
+        "Graded Quiz",
+        "Practice Quiz",
+        "Graded Survey",
+        "Ungraded Survey",
+      ],
+      default: "Graded Quiz",
+    },
     point: { type: Number, default: 0 },
-    status: { type: String, enum: ["Published", "Unpublished"], default: "Unpublished" },
+    status: {
+      type: String,
+      enum: ["Published", "Unpublished"],
+      default: "Unpublished",
+    },
     assignmentGroup: { type: String, default: "Quizzes" },
     shuffleAnswer: { type: String, enum: ["Yes", "No"], default: "Yes" },
     timeLimit: { type: Number, default: 20 }, // 单位：分钟
@@ -15,10 +32,14 @@ const schema = new mongoose.Schema(
     showCorrectAnswers: { type: String, default: "Immediately" },
     oneQuestionAtATime: { type: String, enum: ["Yes", "No"], default: "Yes" },
     webcamRequired: { type: String, enum: ["Yes", "No"], default: "No" },
-    lockQuestionsAfterAnswering: { type: String, enum: ["Yes", "No"], default: "No" },
+    lockQuestionsAfterAnswering: {
+      type: String,
+      enum: ["Yes", "No"],
+      default: "No",
+    },
     dueDate: { type: Date, required: true },
     availableDate: { type: Date, required: true },
-    untilDate: { type: Date, required: true }
+    untilDate: { type: Date, required: true },
   },
   { collection: "quizzes", timestamps: true }
 );
